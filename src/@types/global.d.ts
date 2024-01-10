cc737e2e1d155eb8d0134ee0952e39e50d07fcc3
Jung Wook Park <hatchling3713@gmail.com>
2024-01-10 14:39:59 +0900

    working on with tinaCMS setup

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -6,8 +6,14 @@ declare global {
       TINA_PUBLIC_IS_LOCAL: string;
 
       GITHUB_BRANCH: string;
+      GITHUB_OWNER: string;
+      GITHUB_REPO: string;
+      GITHUB_PERSONAL_ACCESS_TOKEN: string;
 
       VERCEL_GIT_COMMIT_REF: string;
+
+      KV_REST_API_URL: string;
+      KV_REST_API_TOKEN: string;
     }
   }
 }
