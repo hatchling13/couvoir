f669c325e26ddf9b215b27c4b5033acba34b26f0
Jung Wook Park <hatchling3713@gmail.com>
2024-06-27 17:59:23 +0900

    working on with blog

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+});
+const retrospectCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+  retrospect: retrospectCollection,
+};
