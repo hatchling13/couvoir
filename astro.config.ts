ee40909bb2db39468135f15c570109eff6ac29f9
Jung Wook Park <hatchling3713@gmail.com>
2024-07-05 01:04:50 +0900

    Add support for mathematical equations

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'astro/config';
+
 import vercel from '@astrojs/vercel/serverless';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
+
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import rehypeShiftHeading from 'rehype-shift-heading';
+import type { RehypePlugins, RemarkPlugins } from 'astro';
+
+const remarkPlugins: RemarkPlugins = [remarkMath];
+const rehypePlugins: RehypePlugins = [
+  [rehypeKatex, { strict: true }],
+  [rehypeShiftHeading, { shift: 1 }],
+];
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [icon()]
+  integrations: [icon()],
+  markdown: {
+    remarkPlugins,
+    rehypePlugins,
+  },
 });
