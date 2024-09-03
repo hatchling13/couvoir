1cd5b29f863e1273ceda570d34a0fed27346bdb3
Jung Wook Park <hatchling3713@gmail.com>
2024-09-04 00:15:32 +0900

    update astro modify article content style refactor content/config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,32 +1,19 @@
 import { z, defineCollection } from 'astro:content';
 
-const postCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.string().date().transform((d) => new Date(d)),
-    taxonomies: z.object({
-      categories: z.array(z.string()),
-      tags: z.array(z.string()),
-    }),
-  }),
-});
-
-const retrospectCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.string().date().transform((d) => new Date(d)),
-    taxonomies: z.object({
-      categories: z.array(z.string()),
-      tags: z.array(z.string()),
-    }),
+const postSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  date: z
+    .string()
+    .date()
+    .transform((d) => new Date(d)),
+  taxonomies: z.object({
+    categories: z.array(z.string()),
+    tags: z.array(z.string()),
   }),
 });
 
 export const collections = {
-  post: postCollection,
-  retrospect: retrospectCollection,
+  post: defineCollection({ schema: postSchema }),
+  retrospect: defineCollection({ schema: postSchema }),
 };
